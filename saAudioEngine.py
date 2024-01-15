df4a5efe55ef1710116184c71779e19b346bb453
Jared Durham <jared@jdurham.me>
2024-01-14 22:13:37 -0600

    Reformat with Black

--- saAudioEngine.py
+++ saAudioEngine.py
@@ -1,23 +1,21 @@
 import os
+
+import numpy as np
 import sounddevice as sd
 import soundfile as sf
-import numpy as np
+
 assert np
-import wave
-import shutil
-import filecmp
 import requests
 import json
 import tempfile
 import queue
 import sys
-import time
 from requests import Session
-from threading import Thread
 from pydub import AudioSegment
 from dotenv import load_dotenv
 
 import logging
+
 l = logging.getLogger("pydub.converter")
 l.setLevel(logging.ERROR)
 l.addHandler(logging.StreamHandler())
@@ -26,14 +24,26 @@ l.addHandler(logging.StreamHandler())
 _session = Session()
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(BASEDIR, '.env'))
+load_dotenv(os.path.join(BASEDIR, ".env"))
 
-SA_API_KEY = os.getenv('SA_API_KEY')
+SA_API_KEY = os.getenv("SA_API_KEY")
 
 q = queue.Queue()
 
+
 class AudioHandler:
-    def __init__(self, fileName="", saUpload=None, fullTitle="", speakerName="", publishTimestamp="", preachDate="", eventType="", bibleText="", series=""):
+    def __init__(
+        self,
+        fileName="",
+        saUpload=None,
+        fullTitle="",
+        speakerName="",
+        publishTimestamp="",
+        preachDate="",
+        eventType="",
+        bibleText="",
+        series="",
+    ):
         self.fileName = fileName
         self.is_recording = False
         self.saUpload = saUpload
@@ -44,6 +54,7 @@ class AudioHandler:
         self.eventType = eventType
         self.bibleText = bibleText
         self.series = series
+
     def recCallback(self, indata, frames, time, status):
         if status:
             print(status, file=sys.stderr)
@@ -51,39 +62,51 @@ class AudioHandler:
 
     def recordAudio(self):
         device_info = sd.query_devices(kind="input")
-        dev = device_info['index']
-        channels = device_info['max_input_channels']
-        fs = int(device_info['default_samplerate'])
-        print(f"Device Name: {device_info['name']}\nChannels: {channels}\nSample Rate: {fs}")
+        dev = device_info["index"]
+        channels = device_info["max_input_channels"]
+        fs = int(device_info["default_samplerate"])
+        print(
+            f"Device Name: {device_info['name']}\nChannels: {channels}\nSample Rate: {fs}"
+        )
         try:
-            tmpFile = tempfile.mktemp(prefix='delme_rec_unlimited_',
-                                            suffix='.wav', dir='')
+            tmpFile = tempfile.mktemp(
+                prefix="delme_rec_unlimited_", suffix=".wav", dir=""
+            )
             self.tmpFile = tmpFile
-            with sf.SoundFile(tmpFile, mode='x', samplerate=fs,
-                            channels=channels) as file:
-                with sd.InputStream(samplerate=fs, device=dev,
-                                    channels=channels, callback=self.recCallback):
+            with sf.SoundFile(
+                tmpFile, mode="x", samplerate=fs, channels=channels
+            ) as file:
+                with sd.InputStream(
+                    samplerate=fs,
+                    device=dev,
+                    channels=channels,
+                    callback=self.recCallback,
+                ):
                     while self.is_recording:
                         file.write(q.get())
         except KeyboardInterrupt:
-            print('\nRecording finished: ' + repr(tmpFile))
+            print("\nRecording finished: " + repr(tmpFile))
         finally:
             self.saveAudio()
-        
+
     def saveAudio(self):
         audio = AudioSegment.from_wav(f"{self.tmpFile}")
 
         fade_in_duration = 5000  # in milliseconds
         fade_out_duration = 5000  # in milliseconds
         audio = audio.fade_in(fade_in_duration).fade_out(fade_out_duration)
-        outFile=f"{os.getenv('AUDIO_DIR')}{self.fileName}.mp3"
+        outFile = f"{os.getenv('AUDIO_DIR')}{self.fileName}.mp3"
         self.outFile = outFile
-        audio.export(f"{outFile}", format="mp3", tags={
-            'title': f"{self.fullTitle}",
-            'artist': f"{self.speakerName}",
-            'album': f"{self.bibleText}",
-            'comment': f"{self.series}"
-        })
+        audio.export(
+            f"{outFile}",
+            format="mp3",
+            tags={
+                "title": f"{self.fullTitle}",
+                "artist": f"{self.speakerName}",
+                "album": f"{self.bibleText}",
+                "comment": f"{self.series}",
+            },
+        )
         os.remove(f"{self.tmpFile}")
         if self.saUpload:
             self.createSermon()
@@ -97,13 +120,13 @@ class AudioHandler:
         print(f"preachDate: {self.preachDate}")
         print(f"eventType: {self.eventType}")
         print(f"bibleText: {self.bibleText}")
-        
+
         url = "https://api.sermonaudio.com/v2/node/sermons"
-        
+
         headers = {
             "accept": "application/json",
             "X-API-Key": SA_API_KEY,
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
         }
 
         sermon_data = {
@@ -119,14 +142,14 @@ class AudioHandler:
             "eventType": f"{self.eventType}",
             "bibleText": f"{self.bibleText}",
             "moreInfoText": "",
-            "keywords": ""
+            "keywords": "",
         }
 
         try:
             response = requests.post(url, headers=headers, data=json.dumps(sermon_data))
 
             if response.status_code == 200 or 201:
-                sermon_id = response.json().get('sermonID')
+                sermon_id = response.json().get("sermonID")
                 self.sermonId = sermon_id
                 print(f"Sermon created successfully with ID: {sermon_id}")
                 self.createMedia()
@@ -139,24 +162,24 @@ class AudioHandler:
 
     def createMedia(self):
         url = "https://api.sermonaudio.com/v2/media"
-        
+
         headers = {
             "accept": "application/json",
             "X-API-Key": SA_API_KEY,
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
         }
 
         data = {
             "uploadType": "original",
             "sermonID": f"{self.sermonId}",
-            "originalFilename": f"{self.fileName}"
+            "originalFilename": f"{self.fileName}",
         }
 
         try:
             response = requests.post(url, headers=headers, data=json.dumps(data))
             if response.status_code == 200 or 201:
                 print(response)
-                uploadURL = response.json().get('uploadURL')
+                uploadURL = response.json().get("uploadURL")
                 self.uploadURL = uploadURL
                 print(f"Upload URL: {uploadURL}")
                 self.uploadMedia()
@@ -165,15 +188,14 @@ class AudioHandler:
                 print(response.text)
         except Exception as e:
             print(f"Error: {e}")
-    
+
     def uploadMedia(self):
         url = f"{self.uploadURL}"
         file_path = f"{self.outFile}"
-        
-        
+
         header = {"X-API-Key": SA_API_KEY}
 
-        file = {'file': open(file_path, 'rb')}
+        file = {"file": open(file_path, "rb")}
         with open(file_path, "rb") as fp:
             try:
                 response = _session.post(url, data=fp, stream=True, headers=header)
