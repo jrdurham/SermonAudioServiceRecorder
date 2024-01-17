fdee6950a05fc8b4aed84ebaea0a87939b39ce8c
Jared Durham <jared@jdurham.me>
2024-01-17 09:10:46 -0600

    Reformat with Black

--- saAudioEngine.py
+++ saAudioEngine.py
@@ -1,16 +1,19 @@
 import os
-import numpy as np
 import queue
-import saapi
-import sounddevice as sd
-import soundfile as sf
 import sys
 import tempfile
 from datetime import datetime
 from pathlib import Path
+
+import numpy as np
+import sounddevice as sd
+import soundfile as sf
 from pydub import AudioSegment
 from requests import Session
+
+import saapi
 from sasrconfig import config
+
 assert np
 
 _session = Session()
@@ -51,44 +54,47 @@ def api_list():
     return sd_api_list
 
 
+def rec_callback(indata, frames, time, status):
+    if status:
+        print(status, file=sys.stderr)
+    q.put(indata.copy())
+
+
 class AudioHandler:
     def __init__(
         self,
         sar_instance,
-        fileName="",
-        saUpload=None,
-        fullTitle="",
-        speakerName="",
-        preachDate="",
-        eventType="",
-        bibleText="",
+        file_name="",
+        sa_upload=None,
+        full_title="",
+        speaker_name="",
+        preach_date="",
+        event_type="",
+        bible_text="",
         series="",
-        sa_status=False
+        sa_status=False,
     ):
         self.sar = sar_instance
-        self.fileName = fileName
+        self.file_name = file_name
         self.is_recording = False
-        self.saUpload = saUpload
-        self.fullTitle = fullTitle
-        self.speakerName = speakerName
-        self.preachDate = preachDate
-        self.eventType = eventType
-        self.bibleText = bibleText
+        self.sa_upload = sa_upload
+        self.full_title = full_title
+        self.speaker_name = speaker_name
+        self.preach_date = preach_date
+        self.event_type = event_type
+        self.bible_text = bible_text
         self.series = series
         self.sa_status = sa_status
+        self.tmp_file = None
+        self.outfile = None
 
     def message(self, info):
         self.sar.write_console(f"[AudioEngine] {info}")
         print(f"[AudioEngine] {info}")
 
-    def recCallback(self, indata, frames, time, status):
-        if status:
-            print(status, file=sys.stderr)
-        q.put(indata.copy())
-
-    def recordAudio(self):
+    def record_audio(self):
         if "AUDIO_DEVICE" in config():
-            audio_dev_id = int(str(config()["AUDIO_DEVICE"]).split(':')[0])
+            audio_dev_id = int(str(config()["AUDIO_DEVICE"]).split(":")[0])
             device_info = sd.query_devices(device=audio_dev_id)
         else:
             device_info = sd.query_devices(kind="input")
@@ -97,32 +103,33 @@ class AudioHandler:
         channels = device_info["max_input_channels"]
         fs = int(device_info["default_samplerate"])
         self.message(
-            f"Audio Device Information:\n\n  Device Name: {device_info['name']}\n  Channels: {channels}\n  Sample Rate: {fs}\n"
+            f"Audio Device Information:\n\n  "
+            f"Device Name: {device_info['name']}\n  "
+            f"Channels: {channels}\n  "
+            f"Sample Rate: {fs}\n"
         )
-        tmpFile = tempfile.mktemp(
-            prefix="temp_saae", suffix=".wav", dir=""
-        )
-        self.tmpFile = tmpFile
+        tmp_file = tempfile.mktemp(prefix="temp_saae", suffix=".wav", dir="")
+        self.tmp_file = tmp_file
         try:
             with sf.SoundFile(
-                tmpFile, mode="x", samplerate=fs, channels=channels
+                tmp_file, mode="x", samplerate=fs, channels=channels
             ) as file:
                 with sd.InputStream(
                     samplerate=fs,
                     device=dev,
                     channels=channels,
-                    callback=self.recCallback,
+                    callback=rec_callback,
                 ):
                     self.message("Recording audio.")
                     while self.is_recording:
                         file.write(q.get())
         except KeyboardInterrupt:
-            self.message("Recording finished: " + repr(tmpFile))
+            self.message("Recording finished: " + repr(tmp_file))
         finally:
-            self.saveAudio()
+            self.save_audio()
             self.message("Ready to record.")
 
-    def saveAudio(self):
+    def save_audio(self):
         audio_path = config()["AUDIO_PATH"]
         self.message("Recording stopped. Beginning file export.")
         self.message("Validating destination path.")
@@ -131,50 +138,66 @@ class AudioHandler:
             try:
                 Path(f"{audio_path}").mkdir(parents=True, exist_ok=True)
             except FileExistsError:
-                self.message(f"Unable to create {config()["AUDIO_PATH"]} directory. Saving to repo directory.")
+                self.message(
+                    f"Unable to create {audio_path} directory. Saving to repo directory."
+                )
                 audio_path = "."
+            else:
+                self.message(f"Created {audio_path} directory, saving file.")
+        else:
+            self.message("Destination path valid, saving audio file.")
 
-        audio = AudioSegment.from_wav(f"{self.tmpFile}")
+        audio = AudioSegment.from_wav(f"{self.tmp_file}")
         fade_in_duration = 5000  # in milliseconds
         fade_out_duration = 5000  # in milliseconds
         audio = audio.fade_in(fade_in_duration).fade_out(fade_out_duration)
-        corrected_file_name = str(f"{self.fileName}").replace(' - ', ' ')
-        if config()["APPEND_TIMESTAMP"] == "TRUE":
-            corrected_file_name = f"{corrected_file_name}_{int(round(datetime.now().timestamp()))}"
-        outFile = f"{audio_path}/{corrected_file_name}.mp3"
-        self.outFile = outFile
+        corrected_file_name = str(f"{self.file_name}").replace(" - ", " ")
+        if config()["APPEND_TIMESTAMP"] == "TRUE" or os.path.isfile(
+            f"{audio_path}/{corrected_file_name}.mp3"
+        ):
+            corrected_file_name = (
+                f"{corrected_file_name}_{int(round(datetime.now().timestamp()))}"
+            )
+        outfile = f"{audio_path}/{corrected_file_name}.mp3"
+        self.outfile = outfile
         audio.export(
-            f"{outFile}",
+            f"{outfile}",
             format="mp3",
             tags={
-                "title": f"{self.fullTitle}",
-                "artist": f"{self.speakerName}",
-                "album": f"{self.bibleText}",
+                "title": f"{self.full_title}",
+                "artist": f"{self.speaker_name}",
+                "album": f"{self.bible_text}",
                 "comment": f"{self.series}",
             },
         )
-        self.message(f"Audio File: {outFile}")
-        os.remove(f"{self.tmpFile}")
-        if self.saUpload:
+        self.message(f"Audio File: {outfile}")
+        os.remove(f"{self.tmp_file}")
+        if self.sa_upload:
             if not self.sa_status:
-                self.message("Sermon marked for SermonAudio upload, but configuration is invalid. Skipping upload.")
-            elif not self.fullTitle:
-                self.message("Sermon marked for upload, but the Title field is empty (required). Skipping upload.")
-            elif not self.speakerName:
-                self.message("Sermon marked for upload, but the Speaker field is empty (required). Skipping upload.")
-            elif self.saUpload and self.sa_status:
+                self.message(
+                    "Sermon marked for SermonAudio upload, but configuration is invalid. Skipping upload."
+                )
+            elif not self.full_title:
+                self.message(
+                    "Sermon marked for upload, but the Title field is empty (required). Skipping upload."
+                )
+            elif not self.speaker_name:
+                self.message(
+                    "Sermon marked for upload, but the Speaker field is empty (required). Skipping upload."
+                )
+            elif self.sa_upload and self.sa_status:
                 self.message("Sermon Marked for SermonAudio upload, beginning process.")
                 sermon_id = saapi.create_sermon(
                     self,
-                    self.fullTitle,
-                    self.speakerName,
-                    self.preachDate,
-                    self.eventType,
-                    self.bibleText,
-                    self.series
+                    self.full_title,
+                    self.speaker_name,
+                    self.preach_date,
+                    self.event_type,
+                    self.bible_text,
+                    self.series,
                 )
 
-                response = saapi.upload_audio(sermon_id, outFile)
+                response = saapi.upload_audio(sermon_id, outfile)
                 if not response:
                     self.message(
                         f"Media upload successful."
